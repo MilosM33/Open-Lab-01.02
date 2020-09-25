a9faa52f4b73c81a77bfb9d5c8fe8bf70b4ca153
MilosM33 <milosmarejka89@gmail.com>
2020-09-25 14:57:40 +0200

    Sum func

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -4,9 +4,7 @@ namespace Open_Lab_01._02
 {
     public class Exercise
     {
-        public int Sum(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
+        public int Sum(int a, int b) => a + b;
+        
     }
 }
